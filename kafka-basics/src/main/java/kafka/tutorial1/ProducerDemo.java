02f3b7c0b6f2fe706c9bb8569cd68a545dbb99fd
Piyush Mittal <m@piyushmittal.com>
2018-09-28 17:13:01 +0700

    code update

--- kafka-basics/src/main/java/kafka/tutorial1/ProducerDemo.java
+++ kafka-basics/src/main/java/kafka/tutorial1/ProducerDemo.java
@@ -1,38 +1,37 @@
 package kafka.tutorial1;
 
+import java.util.Properties;
+
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-import java.util.Properties;
-
 public class ProducerDemo {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        String bootstrapServers = "127.0.0.1:9092";
+		final String bootstrapServers = "192.168.1.48:9092";
 
-        // create Producer properties
-        Properties properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		// create Producer properties
+		final Properties properties = new Properties();
+		properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-        // create the producer
-        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+		// create the producer
+		final KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
-        // create a producer record
-        ProducerRecord<String, String> record =
-                new ProducerRecord<String, String>("first_topic", "hello world");
+		// create a producer record
+		final ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "hello world");
 
-        // send data - asynchronous
-        producer.send(record);
+		// send data - asynchronous
+		producer.send(record);
 
-        // flush data
-        producer.flush();
-        // flush and close producer
-        producer.close();
+		// flush data
+		producer.flush();
+		// flush and close producer
+		producer.close();
 
-    }
+	}
 }
